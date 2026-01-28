c24ec9b5fdbc244bdafcbd8df15cd96dad4682e5
Malishkakotian17 <malishka24kotian@gmail.com>
2026-01-28 11:59:48 +0530

    Added Create Folder

--- CreateFolder.ps1
+++ CreateFolder.ps1
@@ -1,9 +1,1 @@
-$folderPath = "C:\AutomationLab"
-
-if (-not (Test-Path -Path $folderPath)) {
-    # Create the folder if it does not exist
-    New-Item -Path $folderPath -ItemType Directory
-    Write-Host "Folder 'AutomationLab' created at $folderPath"
-} else {
-    Write-Host "Folder 'AutomationLab' already exists at $folderPath"
-}
+mkdir C:\\AutomationLab
